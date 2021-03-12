283773251f74fd94a12f13a01ad4e042a56c21d7
clayton bond <klaybond@yahoo.co.nz>
2021-03-12 21:09:29 +1300

    inital commit

---
+++ 2.2 Hello World/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace _2._2_Hello_World
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayMessage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // displayMessage
+            // 
+            this.displayMessage.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.displayMessage.Location = new System.Drawing.Point(196, 91);
+            this.displayMessage.Name = "displayMessage";
+            this.displayMessage.Size = new System.Drawing.Size(181, 88);
+            this.displayMessage.TabIndex = 0;
+            this.displayMessage.Text = "Display Message";
+            this.displayMessage.UseVisualStyleBackColor = true;
+            this.displayMessage.Click += new System.EventHandler(this.displayMessage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.displayMessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button displayMessage;
+    }
+}
+
